509c9264cb1a4af2738c168ab2a1f05ddef195db
A. Calleja <albacalpa@gmail.com>
2022-09-01 14:24:18 +0200

    create vue project

---
+++ my-lab/src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>This s an empty footer</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ my-lab/src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>This is an empty navbar
+  </p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
